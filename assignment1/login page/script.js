71489ee560c8218c0299772505c37ae5255e5381
Code Crunch <144154013+paricharm@users.noreply.github.com>
2024-10-01 18:53:03 +0530

    Add files via upload

---
+++ assignment1/login page/script.js
@@ -0,0 +1,14 @@
+// document.getElementById('loginForm').addEventListener('submit', function(event) {  
+//     event.preventDefault();  
+//     const onlineId = document.getElementById('user').value;  
+//     const password = document.getElementById('pass').value;  
+//     const rememberMe = document.getElementById('rememberMe').checked;  
+
+//     // Here you can add your login logic  
+//     console.log('Online ID:', onlineId);  
+//     console.log('Password:', password);  
+//     console.log('Remember Me:', rememberMe);  
+    
+//     // For demonstration, just alerting the user  
+//     alert('Login submitted!');  
+// });
